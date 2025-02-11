1d0f7a6caa65e373ef847ac5c89b71eed7175101
kwyze <104262232+Kwyze@users.noreply.github.com>
2025-02-11 13:53:17 +0300

    reverse_cod_with_vector

---
+++ reverse_cod_with_vector.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string reverseString(string inputString) { // добавляет reverseString, которая принимает inputString как аргумент и возвращает перевернутую строку
+	vector<char> charVector(inputString.begin(), inputString.end()); // добавляет charVector и инициализирует его копированием всех символов из строки inputString
+	reverse(charVector.begin(), charVector.end()); // Переворачивает charVector с помощью reverse
+	return string(charVector.begin(), charVector.end()); // новая строка из перевернутого charVector и возвращает её
+}
+
+int main() {
+	string text;
+
+	cout << "Введите текст: ";
+	getline(cin, text);
+
+	string reversedText = reverseString(text);
+
+	cout << "Текст задом наперед: " << reversedText << endl;
+
+	return 0;
+}
