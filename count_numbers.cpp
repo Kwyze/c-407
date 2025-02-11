f334f24d2ed108f7f47bb8d8dbf9a0b2d56f09e4
kwyze <104262232+Kwyze@users.noreply.github.com>
2025-02-11 13:25:50 +0300

    count_numbers

---
+++ count_numbers.cpp
@@ -0,0 +1,21 @@
+﻿include <iostream>
+#include <string>
+
+using namespace std;
+
+int razmer() {
+	string stroka;
+
+	cout << "Введите строку: ";
+	getline(cin, stroka); // считываем строку целиком, включая пробелы
+
+	return stroka.length(); // возвращаем длину строки
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int size = razmer();
+	cout << "Размер введенной с  троки: " << size << endl;
+
+	return 0;
+}
